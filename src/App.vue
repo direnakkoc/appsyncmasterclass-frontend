79cb1eb5cc822535386b07618e65de54238b8adb
direnakkoc <drnakkoc94@gmail.com>
2022-11-01 09:09:33 +0000

    Initial commit of frontend project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <amplify-authenticator usernameAlias="email">
+      <amplify-sign-up
+        slot="sign-up"
+        :form-fields.prop="signUpFields"
+      ></amplify-sign-up>
+      <div>
+        <h1 v-if="user && user.attributes">Hey, {{user.attributes.name}}!</h1>
+        <amplify-sign-out/>
+      </div>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      user: { },
+      signUpFields: [
+        { type: 'username', label: 'Username (Email) *', required: true },
+        { type: 'password', label: 'Password *', required: true },
+        { type: 'name', label: 'Name *', required: true },
+        { type: 'phone_number', label: 'Phone Number *', required: true },
+      ]
+    }
+  },
+  created(){
+    onAuthUIStateChange((state, user) => {
+      if (state === AuthState.SignedIn) {
+        this.user = user;
+        console.log(user);
+      }
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-import Vue from 'vue'
+import * as Vue from 'vue';
 import App from './App.vue'
+import './assets/tailwind.css'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+
+Amplify.configure({
+  Auth: {
+    region: 'eu-west-1',
+    userPoolId: 'eu-west-1_uXWUsKNA0',
+    userPoolWebClientId: '3fn95d1vnpcgd54i2bop97ior3',
+    mandatorySignIn: true
+  }
+})
+
+const myAppConfig = {
+  'aws_appsync_graphqlEndpoint': 'https://iibjuum47bcpvd3yitrkuopbxq.appsync-api.eu-west-1.amazonaws.com/graphql',
+  'aws_appsync_region': 'eu-west-1',
+  'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS'
+}
+
+Amplify.configure(myAppConfig);
 
 Vue.config.productionTip = false
 
