234de2988fa94a34350e4ccb2c4af654be9e85d2
direnakkoc <drnakkoc94@gmail.com>
2022-11-07 11:19:52 +0000

    Complete whole sign up process

---
+++ src/directives/index.js
@@ -0,0 +1,10 @@
+import escape from './escape.directive';
+// import scroll from './scroll.directive';
+// import linkify from './linkify.directive';
+
+export default {
+  install(Vue) {
+    Vue.directive('escape', escape);
+    
+  }
+}
